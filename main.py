165e892d29865ddc4fc1ae9bbd3135ad60974c81
John <averagehero2@gmail.com>
2022-07-22 16:25:54 -0400

    Faster image loading
    Increased threads used for async image loading

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from Scrapers.tmbd import TmbdScraper
+
 from Pages.SearchPage import SearchPage
 from Pages.LoginPage import LoginPage
 from Pages.SeasonsPage import SeasonsPage
 from Pages.EpisodesPage import EpisodePage
+
 from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivy.core.window import Window
 from kivy.app import App
+from kivy.loader import Loader
 
 class DiscountPopcornTime(App):
 
@@ -14,7 +17,6 @@ class DiscountPopcornTime(App):
         sm = ScreenManager(transition=NoTransition())
         # sm.
         # sm.switch_to("")
-        Window.size = (1550, 920)
         sm.add_widget(LoginPage(scraper, name='login'))
         sm.add_widget(SearchPage(scraper, name='search'))
         sm.add_widget(SeasonsPage(scraper, name='seasons'))
@@ -23,4 +25,6 @@ class DiscountPopcornTime(App):
  
  
 if __name__ == "__main__":
-   DiscountPopcornTime().run()
+    Loader.num_workers = 20 # Maximize thread usage to load all images as fast as possible
+    Window.size = (1550, 920)
+    DiscountPopcornTime().run()
